4b794e0330efda0825326be347437858d0f792aa
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-03-15 20:32:36 -0400

    commit tree 00fd7b619ccc815a1f0db5a8e937d1812e3379a7

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: {:is => 10}
+  
+    # Author phone numbers are exactly ten digits
+
+
+   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in:['Fiction', 'Non-Fiction']}
+    validate :title_is_clickbaity
+
+    def title_is_clickbaity
+        if title
+        unless title.match?(/Won't Believe/ || /Secret/ || /Top/ || /Guess/)
+            errors.add(:title, "Title isn't clickbait-y enough!")
+        end
+    end
+end
 end
